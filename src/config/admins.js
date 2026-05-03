c30ef73de3e3f88083cccfe7145483441cb78b5f
nmphuong91 <nmphuong91@gmail.com>
2026-05-03 14:54:27 +0700

    Up admin

---
+++ src/config/admins.js
@@ -0,0 +1,9 @@
+/**
+ * Developer/admin user IDs with elevated in-game privileges.
+ * These users bypass certain gameplay restrictions (e.g. HP floor, combat deaths).
+ */
+const ADMIN_IDS = new Set([
+	'275992469764833280',
+]);
+
+module.exports = { ADMIN_IDS };
